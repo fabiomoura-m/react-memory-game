ff4b4dfe2bcb9a46680530ba9462f11e123701e9
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-09-02 11:28:57 -0300

    setting up the environment
    install styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import * as C from './App.styles';
+import LogoImage from './assets/devmemory_logo.png';
+
 export function App() {
-    return <div>...</div>;
+    return (
+        <C.Container>
+            <C.Info>
+                <C.LogoLink href=""></C.LogoLink>
+
+                <C.InfoArea>...</C.InfoArea>
+            </C.Info>
+            <C.GridArea></C.GridArea>
+        </C.Container>
+    );
 }
